e3f7b945a7e6468084b72caa5676bb4eb8b32ab6
Jamile Yaacob <jamile.yaacob@student.rocva.nl>
2021-10-28 10:06:57 +0200

    opdracht 5

--- Opdracht5/opdracht_05.py
+++ Opdracht5/opdracht_05.py
@@ -1,3 +1,11 @@
 #maak hier een functie voor een top 3 van bijvoorbeeld je favoriete games, eten of iets anders
 #elke keer dat je de functie aanroept voeg je een nieuwe game toe als parameter
 #weet je niet meer hoe? Check het solution filmpje op: https://www.linkedin.com/learning/programming-foundations-fundamentals-3/solution-favorite-cities
+
+def favorite_games(name):
+    print("Welke zijn je favoriete games?", name)
+
+favorite_games("The sims")
+favorite_games("Skyrim")
+favorite_games("Dragon Age")
+
